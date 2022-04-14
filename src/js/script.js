11340cee2c0fab97b8832860ceecaeb88c99d172
DED73 <dde73@ya.ru>
2022-04-14 08:12:03 +0400

    Begin new_slick

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplaySpeed: 3500,
+        autoplay: true,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/carousel/arrow_left.svg" alt="prev"></button>',
+        nextArrow:'<button type="button" class="slick-next"> <img src="img/carousel/arrow_right.svg" alt="next"></button>'
+        
     });
   });
+
+//   <img src='img/carousel/arrow_right.svg'></img>
