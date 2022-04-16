d4af335df5a9485df0e5af7ede415fb9fdcb4cf4
DED73 <dde73@ya.ru>
2022-04-16 08:46:01 +0400

    Slick slider before adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,42 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed: 3500,
-        autoplay: true,
-        // adaptiveHeight: true,
+        
         prevArrow: '<button type="button" class="slick-prev"> <img src="img/carousel/arrow_left.svg" alt="prev"></button>',
-        nextArrow:'<button type="button" class="slick-next"> <img src="img/carousel/arrow_right.svg" alt="next"></button>'
+        nextArrow:'<button type="button" class="slick-next"> <img src="img/carousel/arrow_right.svg" alt="next"></button>',
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              arrows:false,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows:false,
+              dots: true,
+              slidesToShow: 3,
+              slidesToScroll: 1
+             
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              arrows:false,
+              dots: true,
+              slidesToShow: 3,
+              slidesToScroll: 1
+             
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
         
     });
   });
 
-//   <img src='img/carousel/arrow_right.svg'></img>
