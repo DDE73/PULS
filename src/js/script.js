5f96966e20e8e8f65be4ceb249793cc81569ad78
DED73 <dde73@ya.ru>
2022-04-19 08:47:15 +0400

    8-46 190422 catalog-tabs 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,33 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
-        
         prevArrow: '<button type="button" class="slick-prev"> <img src="img/carousel/arrow_left.svg" alt="prev"></button>',
         nextArrow:'<button type="button" class="slick-next"> <img src="img/carousel/arrow_right.svg" alt="next"></button>',
         responsive: [
           {
-            breakpoint: 991,
+            breakpoint: 1091,
             settings: {
               arrows:false,
               dots: true
               
             }
           },
+
           {
             breakpoint: 768,
             settings: {
               arrows:false,
               dots: true
-              // slidesToShow: 3,
-              // slidesToScroll: 1
-             
+            
             }
           },
+
           {
             breakpoint: 576,
             settings: {
               arrows:false,
               dots: true
-              // slidesToShow: 3,
-              // slidesToScroll: 1
-             
+   
             }
           }
           // You can unslick at a given breakpoint now by adding:
